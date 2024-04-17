d09c3fb80fa29a2125fba1cdc7d91bce8ae2b61c
stevenmorison <stevenmorizon123@gmail.com>
2024-04-17 10:13:16 +0700

    [UPD] split function

--- wizard/mrp_split_workorder.py
+++ wizard/mrp_split_workorder.py
@@ -22,65 +22,35 @@ class MrpSplitWorkOrder(models.TransientModel):
     """function for split the work order into smaller based on the qty_to_split.
     the logic in this function still need to be fix."""
 
-    # def action_split_workorder(self):
-    #     new_workorders = []
-        
-    #     total_qty_to_produce = self.production_id.product_qty 
-        
-    #     for i in range(self.qty_to_split):
-    #         new_name = self.workorder_id.copy(default={
-    #             'name': '%s (Split %s)' % (self.workorder_id.name, i + 1),
-    #             'product_qty': int(total_qty_to_produce / self.qty_to_split),})
-
-    #         new_workorders.append(new_name.id)
-    #     self.production_id.qty_producing -= self.qty_to_split
-        
-    #     qty_per_workorder = total_qty_to_produce / self.qty_to_split
-
-    #     for new_workorder in self.env['mrp.workorder'].browse(new_workorders):
-    #         new_workorder.state = 'ready'
-    #         new_workorder.product_qty = qty_per_workorder
-
-    #     return {
-    #         'name': 'Work Orders',
-    #         'type': 'ir.actions.act_window',
-    #         'res_model': 'mrp.workorder',
-    #         'view_mode': 'tree,form',
-    #         'domain': [('id', 'in', new_workorders)],
-    #     }
-
     def action_split(self):
         workorders_baru = []
         for record in self:
+            total_qty_seluruh = record.product_qty
+            jumlah_split = record.qty_to_split
+            kapasitas_per_wo = int(total_qty_seluruh // jumlah_split)
 
-            total_qty_seluruh = product_qty
-            jumlah_split = qty_to_split
-
-            #pembagian kapasitas produksi menggunakan modulo
-            kapasitas_per_wo = int(total_qty_seluruh % jumlah_split)
-
-            for i in range(qty_to_split):
-                nama_baru = workorder_id.copy(default={
-                    'name' : '%s (Splited %s)' % (self.workorder_id.nama_baru, i + 1),
-                    'product_qty' : kapasitas_per_wo,
+            for i in range(jumlah_split):
+                nama_baru = record.workorder_id.copy(default={
+                    'name': '%s (Splited %s)' % (record.workorder_id.name, i + 1),
+                    'product_qty': kapasitas_per_wo,
                 })
+                workorders_baru.append(nama_baru.id)
+                print(f"Work Order baru {i+1}: product_qty = {kapasitas_per_wo}") # Debugging
 
-            workorders_baru.append(nama_baru.id)
-            production_id.qty_producing -= jumlah_split  
-            
-            for workorder_baru in self.env['mrp.workorder'].browse(workorders_baru): 
-                workorder_baru.state = 'ready'
-                workorder_baru.product_qty = kapasitas_per_wo
+            record.production_id.qty_producing -= jumlah_split
 
-            return {
-                'name' : 'Work Orders',
-                'type' : 'ir.actions.act_window',
-                'res_model' : 'mrp.workorder',
-                'view_mode' : 'tree, form',
-                'domain' : [('id', 'in', workorders_baru)],
+        for workorder_baru in self.env['mrp.workorder'].browse(workorders_baru):
+            workorder_baru.state = 'ready'
+            print(f"Work Order baru: {workorder_baru.name}, product_qty = {kapasitas_per_wo}") #Debugging
 
-            }    
-        
+        return {
+            'name': 'Work Orders',
+            'type': 'ir.actions.act_window',
+            'res_model': 'mrp.workorder',
+            'view_mode': 'tree,form',
+            'domain': [('id', 'in', workorders_baru)],
+        }
+
     @api.depends('production_detailed_vals_ids')
     def _compute_counter(self):
         for wizard in self:
@@ -131,7 +101,5 @@ class MrpProductionSplitLine(models.TransientModel):
     date = fields.Datetime('Schedule Date')
     production_id = fields.Many2one('mrp.production', 'Manufacturing Order')
     product_id = fields.Many2one(related='production_id.product_id')
-
-
 
 
