87af6c3b9f08ca19cac2e98a17cd048f27d8210a
stevenmorison <stevenmorizon123@gmail.com>
2024-05-04 12:54:09 +0700

    [UPD] inherit_mrp_workorder
    [UPD] 04-05-2024
    -> override `button_finish` function from `mrp.workorder` and add logic to find next work order after user click `done`

--- models/inherit_mrp_workorder.py
+++ models/inherit_mrp_workorder.py
@@ -12,36 +12,16 @@ class MrpWorkOrder(models.Model):
     remaining_qty = fields.Float(string='Quantity After Split')
 
     def button_finish(self):
-        res = super(MrpWorkOrder, self).button_finish()
-        end_date = datetime.now()
-        for workorder in self:
-            if workorder.state in ('done', 'cancel'):
-                continue
-            workorder.end_all()
-            vals = {
-                'qty_produced': workorder.qty_produced or workorder.qty_producing or workorder.qty_production,
-                'state': 'done',
-                'date_finished': end_date,
-                'date_planned_finished': end_date
-            }
-            if not workorder.date_start:
-                vals['date_start'] = end_date
-            if not workorder.date_planned_start or end_date < workorder.date_planned_start:
-                vals['date_planned_start'] = end_date
-            workorder.with_context(bypass_duration_calculation=True).write(vals)
+        super(MrpWorkOrder, self).button_finish()
+        production = self.production_id
+        next_work_order = self.find_next_work_order()
+        if next_work_order:
+            next_work_order.button_start()
 
-            # Menambahkan logika untuk menentukan Work Order berikutnya yang "Ready"
-            next_work_order = workorder._get_next_work_order()
-            if next_work_order:
-                next_work_order.state = 'ready'
-
-        return True
-        return res
-
-    def _get_next_work_order(self):
-        # Mengembalikan Work Order berikutnya yang "Ready" untuk dilanjutkan
+    def find_next_work_order(self):
         next_work_order = self.env['mrp.workorder'].search([
             ('production_id', '=', self.production_id.id),
             ('state', '=', 'pending'),
-        ], limit=1, order='sequence')
+            ('workcenter_id', '>', self.workcenter_id.id),
+        ], limit=1, order='workcenter_id')
         return next_work_order
