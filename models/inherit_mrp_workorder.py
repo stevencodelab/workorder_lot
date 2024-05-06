c4530e351f96c234455dc27381ace96096b3fbfd
stevenmorison <stevenmorizon123@gmail.com>
2024-05-06 15:58:41 +0700

    [UPD] add function
    [UPD]
    
    -> add function

--- models/inherit_mrp_workorder.py
+++ models/inherit_mrp_workorder.py
@@ -17,6 +17,13 @@ class MrpWorkOrder(models.Model):
         next_work_order = self.find_next_work_order()
         if next_work_order:
             next_work_order.button_start()
+        else:
+            # Cek apakah ini tahap terakhir dalam produksi
+            if not self.find_pending_work_orders():
+                # Tidak ada tahap produksi berikutnya, restart dari tahap pertama
+                first_work_order = self.find_first_work_order()
+                if first_work_order:
+                    first_work_order.button_start()
 
     def find_next_work_order(self):
         next_work_order = self.env['mrp.workorder'].search([
@@ -25,3 +32,18 @@ class MrpWorkOrder(models.Model):
             ('workcenter_id', '>', self.workcenter_id.id),
         ], limit=1, order='workcenter_id')
         return next_work_order
+
+    def find_first_work_order(self):
+        first_work_order = self.env['mrp.workorder'].search([
+            ('production_id', '=', self.production_id.id),
+            ('state', '=', 'pending'),
+        ], limit=1, order='workcenter_id')
+        return first_work_order
+
+    def find_pending_work_orders(self):
+        # Cek apakah ada tahap produksi yang masih menunggu
+        pending_work_orders = self.env['mrp.workorder'].search([
+            ('production_id', '=', self.production_id.id),
+            ('state', '=', 'pending'),
+        ])
+        return pending_work_orders
